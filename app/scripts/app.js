18f49101ea2e1cbbd57edd5dc744942c1393798e
Val-alt <45761879+Val-alt@users.noreply.github.com>
2019-08-14 17:13:22 +0300

    com july

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,66 +2,33 @@ var App = {
   options: {},
 
   init: function() {
-    // this.navMob();
+    this.navMob();
     this.animateScroll();
-    this.pricesToggle();
+    this.navSticky();
+    this.carousel1 = this.carousel();
   },
 
-  // navMob: function() {
-  //   var btnMenu = document.querySelector(".navigation-mob__btn");
-  //   var blockMenu = document.querySelector(".navigation-mob__block");
-  //   // var arrow = document.querySelector(".arrow");
-  //   var check = 0;
-  //   console.log(check);
-
-  //   btnMenu.onclick = () => {
-  //     console.log(check);
-  //     if (check === 0) {
-  //       // если меню скрыто - открыть
-  //       blockMenu.style.display = "block";
-  //       // arrow.style.transform = "rotate(270deg)";
-  //       check = 1;
-  //     } else {
-  //       // если меню открыто - скрыть
-  //       blockMenu.style.display = "none";
-  //       // arrow.style.transform = "rotate(90deg)";
-  //       check = 0;
-  //     }
-  //   };
-
-  //   document.querySelector(".navigation-mob__block").onmouseleave = () => {
-  //     // скрыть менюшку при уходе мышки
-  //     if (window.matchMedia("(max-width: 900px)").matches) {
-  //       blockMenu.style.display = "none";
-  //       check = 0;
-  //       // arrow.style.transform = "rotate(90deg)";
-  //     }
-  //   };
-
-  //   window.onscroll = function() {
-  //     blockMenu.style.display = "none";
-  //     check = 0;
-  //   };
-  // },
 
   animateScroll: function() {
     $("body").on("click", "a", function(event) {
       var href = event.target.getAttribute("href");
       console.log(href);
       //отменяем стандартную обработку нажатия по ссылке
-      if (href.indexOf("tel") != 0 && href.indexOf("mailto") != 0) {
+      if (
+        href.indexOf("tel") != 0 &&
+        href.indexOf("mailto") != 0 &&
+        !(href.indexOf("html") !== -1)
+      ) {
         event.preventDefault();
 
         //забираем идентификатор бока с атрибута href
         var id = $(this).attr("href"),
           //узнаем высоту от начала страницы до блока на который ссылается якорь
 
-          position = $(id).offset().top;
+          position = $(id).offset().top - 60;
 
         if (window.matchMedia("(max-width: 900px)").matches) {
-          position = position - 60;
-
-          // arrow.style.transform = "rotate(90deg)";
+          position = position - 10;
         }
         console.log(position);
 
@@ -71,52 +38,48 @@ var App = {
     });
   },
 
-  pricesToggle: function() {
+  navMob: function() {
     var animateTime = 300;
 
-    $(".navigation-mob__btn").click(function() {
-      // $(".prices__item-spoiler", this).toggleClass(
-      //   "prices__item-spoiler--active"
-      // );
+    $(".nav-mob__btn").click(function() {
       if (
         $(this)
-          .parents(".navigation-mob")
-          .find(".navigation-mob__block")
+          .parents(".nav-mob")
+          .find(".nav-mob__block")
           .height() === 0
       ) {
         App.autoHeightAnimate(
           $(this)
-            .parents(".navigation-mob")
-            .find(".navigation-mob__block"),
+            .parents(".nav-mob")
+            .find(".nav-mob__block"),
           animateTime
         );
+        $(".nav-mob__btn").css("background-image", "url(../images/close.png)");
       } else {
         $(this)
-          .parents(".navigation-mob")
-          .find(".navigation-mob__block")
+          .parents(".nav-mob")
+          .find(".nav-mob__block")
           .stop()
           .animate({ height: "0" }, animateTime);
+          $(".nav-mob__btn").css("background-image", "url(../images/menu.png)");
       }
     });
 
-    document.querySelector(".navigation-mob__block").onmouseleave = () => {
+    document.querySelector(".nav-mob__block").onmouseleave = () => {
       // скрыть менюшку при уходе мышки
       if (window.matchMedia("(max-width: 900px)").matches) {
-        $(".navigation-mob__block")
+        $(".nav-mob__block")
           .stop()
           .animate({ height: "0" }, animateTime);
+          $(".nav-mob__btn").css("background-image", "url(../images/menu.png)");
       }
     };
 
     window.onscroll = function() {
-      // $(this)
-      //     .parents(".navigation-mob")
-      //     .find(".navigation-mob__block")
-      //     .stop()
-      //     .animate({ height: "0" }, animateTime);
-      $(".navigation-mob__block")
-          .stop()
-          .animate({ height: "0" }, 0);
+      $(".nav-mob__block")
+        .stop()
+        .animate({ height: "0" }, 0);
+        $(".nav-mob__btn").css("background-image", "url(../images/menu.png)");
     };
   },
 
@@ -125,7 +88,72 @@ var App = {
       autoHeight = 200;
     element.height(curHeight);
     element.stop().animate({ height: autoHeight }, time);
+  },
+
+  carousel: function() {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+      var qwe ="";
+      for (var i=1; i<50; i=i+2) {
+        qwe +="<div><img src='images/cats/1 (" + i + ").jpg'/></div><div><img src='images/cats/1 (" + (i+1) + ").jpg'/></div>";
+      }
+    
+      $('.owl-carousel').html(qwe);
+    }
+
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 4,
+      margin: 10,
+      loop: true,
+      nav: true,
+      autoplay : true,
+      navText: [
+        '<img src="../images/arrow.png" alt="Назад" />',
+        '<img src="../images/arrow.png" alt="Вперед" />'
+      ],
+      responsive: {
+        0: {
+          items: 1,
+          autoplay : false
+        },
+        450: {
+          items: 2,
+          autoplay : false
+        },
+        700: {
+          items: 3
+        },
+        918: {
+          items: 4
+        }
+      }
+    });
+  },
+
+  navSticky: function() {
+    $(window).scroll(function() {
+      var animateTime = 200;
+      if ($(".main-page").length) {
+        if ($(this).scrollTop() > 660) {
+          if ($(".nav-sticky").height() === 0) {
+            autoHeightAnimate($(".nav-sticky"), animateTime);
+          }
+        } else {
+          $(".nav-sticky")
+            .stop()
+            .animate({ height: "0" }, 150);
+        }
+      }
+    });
+
+    function autoHeightAnimate(element, time) {
+      var curHeight = element.height(),
+        autoHeight = 72;
+      element.height(curHeight);
+      element.stop().animate({ height: autoHeight }, time);
+    }
   }
+
 };
 
 $(document).ready(function() {
